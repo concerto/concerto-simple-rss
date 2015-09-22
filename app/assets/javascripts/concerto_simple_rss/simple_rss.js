38375e4a7788da1988631569569e111f6749f26f
Marvin Frederickson <marvin.frederickson@gmail.com>
2015-09-21 17:00:39 -0800

    pass credentials and refresh preview on blur

--- app/assets/javascripts/concerto_simple_rss/simple_rss.js
+++ app/assets/javascripts/concerto_simple_rss/simple_rss.js
@@ -12,6 +12,8 @@ function previewSimpleRss() {
   var url = $('input#simple_rss_config_url').data('url');
   if (url) {
     rss_url = $('input#simple_rss_config_url').val();
+    userid = $('input#simple_rss_config_url_userid').val();
+    password = $('input#simple_rss_config_url_password').val();
     output_format = $('select#simple_rss_config_output_format').val();
     max_items = $('input#simple_rss_config_max_items').val();
     reverse_order = $('select#simple_rss_config_reverse_order').val();
@@ -22,6 +24,8 @@ function previewSimpleRss() {
     }
     $("#preview_div").load(url, { data: { 
       url: rss_url, 
+      url_userid: userid,
+      url_password: password,
       output_format: output_format, 
       max_items: max_items,
       reverse_order: reverse_order,
@@ -40,6 +44,8 @@ function initializeSimpleRssHandlers() {
     // on blur and change of url, display format, reverse_order, max items, and xsl
     // not on keyup (poor man's debouncing technique?)
     $('input#simple_rss_config_url').on('blur', previewSimpleRss);
+    $('input#simple_rss_config_url_userid').on('blur', previewSimpleRss);
+    $('input#simple_rss_config_url_password').on('blur', previewSimpleRss);
     $('select#simple_rss_config_output_format').on('change', previewSimpleRss);
     $('input#simple_rss_config_max_items').on('blur', previewSimpleRss);
     $('select#simple_rss_config_reverse_order').on('change', previewSimpleRss);
